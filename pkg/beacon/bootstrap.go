64a06143a428e6db1efc582ddad5efe3d489ca7c
Sam Calder-Mason <sam@puritydev.io>
2025-02-07 09:16:53 +1000

    feat(beacon): refactor HTTP client initialization to use a parameter slice for better flexibility and maintainability feat(beacon): add methods to Options struct for managing GoEth2Client parameters to enhance configurability

--- pkg/beacon/bootstrap.go
+++ pkg/beacon/bootstrap.go
@@ -30,13 +30,16 @@ func (n *node) ensureClients(ctx context.Context) error {
 		default:
 			timeout := 10 * time.Minute
 
-			client, err := ehttp.New(ctx,
+			params := []ehttp.Parameter{
 				ehttp.WithAddress(n.config.Addr),
 				ehttp.WithLogLevel(zerologLevel),
 				ehttp.WithTimeout(timeout),
 				ehttp.WithExtraHeaders(n.config.Headers),
-				ehttp.WithEnforceJSON(true), // TODO(sam): HACK: Remove this once we have a proper spec
-			)
+			}
+
+			params = append(params, n.options.GetGoEth2ClientParams()...)
+
+			client, err := ehttp.New(ctx, params...)
 			if err != nil {
 				failures++
 
